5ef25a2f510b00c450380f9efb163f0b9c4e04de
Rivaroni <michaelrivaroni@gmail.com>
2023-11-01 19:59:05 -0700

    Reads input and outputs correct data

---
+++ README.md
@@ -0,0 +1,10 @@
+# CPSC 335 Project 1: Matching Group Schedules
+
+# Michael Rivera: rivmichael@csu.fullerton.edu
+# Evan Ng: EvanNg21@csu.fullerton.edu
+
+# Description
+This program finds common free time slots for a group of people based on their busy schedules and working hours.
+
+# How to Run
+1. Install Python (version 3.6 or later).
--- project1_starter.py
+++ project1_starter.py
@@ -1,6 +1,18 @@
+import json
+
 def main():
     results = []
-    print("Hello")
+
+    # Read input.txt file and store the data
+    with open("Input.txt", "r") as f:
+        for line in f:
+            data = json.loads(line.strip())
+            results.append(data)
+
+    # Output results into output.txt file in a neat format
+    with open("Output.txt", "w") as f:
+        for result in results:
+            f.write(json.dumps(result, indent=4) + "\n")
 
 if __name__ == "__main__":
     main()
